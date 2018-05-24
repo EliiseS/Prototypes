2996d176d65449eb7ee73c79567bc424e553dbe7
EliiseS <selingeliise@gmail.com>
2018-05-24 19:18:08 +0200

    Create azure service bus

---
+++ ServiceBusApi/ServiceBusApi/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private const string ServiceBusConnectionString = "";
+        private const string QueueName = "queue1";
+        private IQueueClient queueClient;
+
+        public ValuesController()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PutMessage(string message)
+        {
+           var queueMessage = new Message(Encoding.UTF8.GetBytes($"{message} (Added = {DateTime.UtcNow})"));
+
+            // Send the message to the queue.
+            await queueClient.SendAsync(queueMessage);
+            await queueClient.CloseAsync();
+
+            return Content("asdasd");
+        }
+    }
+}
